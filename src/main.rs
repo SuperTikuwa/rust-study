115cb1079604c4f6620dd0ed67a5737af06e6fe9
SuperTikuwa <tikuwamk2@gmail.com>
2021-05-09 00:58:14 +0900

    Part 7

--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,15 @@
-use std::io;
-
-struct Point{
-    x: i32,
-    y: i32,
-}
-
-impl Point{
-    fn print(&self){
-        println!("x:{},y:{}",self.x,self.y);
-    }
+macro_rules! Abs {
+    ($n:expr) => {
+        if $n < 0 {
+            -$n
+        } else {
+            $n
+        }
+    };
 }
 
 fn main() {
+    let n = Abs!(-2);
 
-    let a = vec![1,3,5,7,9];
-    let n = 5;
-    let p = Point{x:1,y:2};
-
-    p.print();
-    match getvalue(&a,n){
-        Some(result) => println!("value {}",result),
-        None => println!("no value {}",n),
-    }
-}
-
-fn getvalue(a:&Vec<i32>, n: i32) -> Option<i32>{
-    for x in a.iter(){
-        if x==&n{
-            return Some(n);
-        }
-    }
-    return None;
+    println!("{}", n);
 }
